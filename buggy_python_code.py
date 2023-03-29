4156a96f3fedd62eb13bb43aad64fead747fc585
Filip Gabriel <xgabrie1@fi.muni.cz>
2023-03-29 09:12:46 +0200

    urllib fix

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -31,7 +31,7 @@ def print_nametag(format_string, person):
 
 def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
-    exec(f"import urllib{urllib_version} as urllib", globals())
+    urllib = exec(f"urllib{urllib_version}", globals())
     # Fetch and print the requested URL
 
     try:
